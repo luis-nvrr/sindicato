6d023b66778e21e38f61f627963b3e8dc2aa9ecd
Luis Navarro <luisfnvrr@gmail.com>
2022-03-09 10:02:02 -0300

    add run length enconding

--- episodes/v1.0/run-length-encoding/src/main.ts
+++ episodes/v1.0/run-length-encoding/src/main.ts
@@ -4,6 +4,23 @@ type RunLengthEncodedString = [string, number][];
 // "aaaabbbbccc" => [["a", 4], ["b", 3], ["c", 3]]
 //
 // see https://en.wikipedia.org/wiki/Run-length_encoding
+//
+// O(2n)
 export function rle(plain: String): RunLengthEncodedString {
-  return [];
+  let charactersCount:Map<string, number> = countCharacters(plain) 
+
+  let result:RunLengthEncodedString = []
+  charactersCount.forEach((count:number, character:string) => result.push([character, count]))
+
+  return result
+}
+
+function countCharacters(plain: String): Map<string, number> {
+  let count = new Map<string, number>()
+  plain.split("").forEach((character:string) => {
+    let currentCount = count.get(character) || 0
+    count.set(character, currentCount+1)
+  });
+
+  return count
 }
